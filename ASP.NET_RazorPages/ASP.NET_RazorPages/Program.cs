46d0ae54841767efdad90f39385e56921d78b80e
SnnBrgmv <sananai@code.edu.az>
2023-11-27 17:29:26 +0400

    Razor page in .NET

---
+++ ASP.NET_RazorPages/ASP.NET_RazorPages/Program.cs
@@ -0,0 +1,18 @@
+namespace ASP.NET_RazorPages
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorPages();
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
